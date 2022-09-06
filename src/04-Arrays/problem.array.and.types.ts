4b77d30e8302d439ebd2df965c9b99cb07e8850e
SaeedAlam <saeedalam@mdmail.nl>
2022-09-07 00:53:58 +0430

    Redesign the course - add some exercises

---
+++ src/04-Arrays/problem.array.and.types.ts
@@ -0,0 +1,28 @@
+
+export type User = unknown;
+
+export const users: unknown[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+
+export function logPerson(user: unknown) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
+
+
+/* In case if you are stuck:
+
+// https://www.typescriptlang.org/docs/handbook/2/objects.html
+*/
